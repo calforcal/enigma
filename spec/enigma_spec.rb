9b917cdae069fb2f83cc8ff60ddd71ee6ab0a8aa
Michael Callahan <mcalla242@gmail.com>
2023-04-16 10:25:55 -0600

    Test: Add helper method testing for enigma class

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -50,6 +50,51 @@ RSpec.describe Enigma do
     end
   end
 
+  describe "#get_value" do
+    it "can get the remainder of a number greater then 27" do
+      expect(@enigma.get_value(5)).to eq(5)
+      expect(@enigma.get_value(32)).to eq(5)
+      expect(@enigma.get_value(1250)).to eq(8)
+    end
+  end
+
+  describe "#under_27" do
+    it "can return the needed shift value for a key and offset" do
+      expect(@enigma.under_27(2, 3)).to eq(5)
+      expect(@enigma.under_27(27, 5)).to eq(5)
+      expect(@enigma.under_27(25, 9)).to eq(7)
+    end
+  end
+
+  describe "#encrypt_message" do
+    it "can encrypt the message and return it as a string" do
+      expect(@enigma.encrypt_message("hello world", "02715", "040895")).to eq("keder ohulw")
+      expect(@enigma.encrypt_message("Michael Callahan", "02715", "040895")).to eq("pivdedtfdedhtg")
+    end
+
+    it "can handle all uppercase" do
+      lower = @enigma.encrypt_message("BEWARE OF ME", "12345", "041623")
+      expect(lower[0]).to eq("r")
+      expect(lower[5]).to eq("b")
+      expect(lower[-1]).to eq("e")
+    end
+
+    it "can return strings with special characters" do
+      special = @enigma.encrypt_message("In! The. Dark - of -- the night: beware", "58903", "041623")
+      expect(special[2]).to eq("!")
+      expect(special[7]).to eq(".")
+    end
+  end
+
+  describe "#shift_a && b && c && d" do
+    it "can shift a given value based on the key given" do
+      expect(@enigma.shift_a("a", 2)).to eq("c")
+      expect(@enigma.shift_b("b", 3)).to eq("e")
+      expect(@enigma.shift_c("c", 4)).to eq("g")
+      expect(@enigma.shift_d("d", 5)).to eq("i")
+    end
+  end
+
   describe "#encrypt" do
     it "can encrypt a message with a key and date" do
       expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(
