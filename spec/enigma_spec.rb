a177af8ebbf82f552325cb1069930e49bc0177ac
Michael Callahan <mcalla242@gmail.com>
2023-04-16 07:26:42 -0600

    Test: Add testing specs for enigma class encrypt method

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,7 +10,43 @@ RSpec.describe Enigma do
     end
 
     it "initializes with an alphabet" do
-      expect(@enigma.alphabet).to eq(["a"])
+      expect(@enigma.alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+    end
+  end
+
+  describe "#key_generator" do
+    it "can generate a key hash with the key given" do
+      expect(@enigma.key_generator("02715")).to eq(
+      {
+        "A": 2,
+        "B": 27,
+        "C": 71,
+        "D": 15
+      })
+    end
+  end
+
+  describe "#offset_generator" do
+    it "can generate a offset hash with the date given" do
+      expect(@enigma.offset_generator("040895")).to eq(
+        {
+          "A": 1,
+          "B": 0,
+          "C": 2,
+          "D": 5
+        })
+    end
+  end
+
+  describe "#final_shift" do
+    it "can return a combined hash of key && offset" do
+      expect(@enigma.final_shift("02715", "040895")).to eq(
+        {
+          "A": 3,
+          "B": 27,
+          "C": 73,
+          "D": 20
+        })
     end
   end
 
